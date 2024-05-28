9ceac002a2af2b78e3ee38e34b4af8ca5f148c68
Amirprogrammer689 <beeline13062015@gmail.com>
2024-05-29 01:29:31 +0400

    Added read and write functions, added json and txt files.

--- lab_3/LAB3/read_or_write_files.py
+++ lab_3/LAB3/read_or_write_files.py
@@ -37,3 +37,37 @@ def read_json_from_file(file_path: str) -> dict:
     except Exception as e:
         print(f"An error has occurred: {e}")
 
+
+def read_binary_from_file(file_path: str) -> bytes:
+    """
+    Read binary data from a file.
+    Parameters:
+    file_path (str): The path to the file to read.
+    Returns:
+    bytes: The binary data read from the file.
+    """
+    try:
+        with open(file_path, 'rb') as file:
+            data = file.read()
+        return data
+    except FileNotFoundError as e:
+        print(f"File not found: {e}")
+    except Exception as e:
+        print(f"An error occurred while reading the file: {e}")
+
+
+def write_binary_to_file(file_path: str, bytes_text: bytes) -> None:
+    """
+    Write binary data to a file.
+    Parameters:
+    file_path (str): The path to the file to write.
+    bytes_text (bytes): The binary data to write to the file.
+    """
+    try:
+        with open(file_path, 'wb') as file:
+            file.write(bytes_text)
+        print("The binary data has been successfully saved to the file.")
+    except FileNotFoundError as e:
+        print(f"File not found: {e}")
+    except Exception as e:
+        print(f"An error occurred while writing the binary data to the file: {e}")
